c14119e802f7eee730c36b82e3dcdea6c956418c
LuiisLaranjeira <luiscarloslaranjeira@hotmail.com>
2025-04-29 09:43:15 +0100

    Multiple Input Files

---
+++ src/serialization.c
@@ -0,0 +1,5 @@
+//
+// Created by ll on 4/29/25.
+//
+
+#include "serialization.h"
---
+++ src/serialization.h
@@ -0,0 +1,8 @@
+//
+// Created by ll on 4/29/25.
+//
+
+#ifndef SERIALIZATION_H
+#define SERIALIZATION_H
+
+#endif //SERIALIZATION_H
